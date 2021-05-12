21c66e29aea2a2b06af33675af692e45d589512e
mkomod <mkomodromos01@gmail.com>
2021-05-12 21:02:54 +0300

    add: horseshoe prior test

--- ssp.R
+++ ssp.R
@@ -5,7 +5,7 @@ omega <- 1
 censoring_lvl <- 0.4
 
 set.seed(1)
-b <- c(1, 1, rep(0, 250))
+b <- c(1, 1, rep(0, 8))
 p <- length(b)
 X <- matrix(rnorm(n * p), nrow=n)
 y <- runif(nrow(X))
@@ -17,7 +17,8 @@ ind_sorted_T <- order(Y)
 ind_failure_T <- which(as.logical(delta[order(Y)]))
 N_delta <- sum(delta)
 
-m.1 <- stan(file="ssp.stan", 
+# laplace prior
+m.1 <- stan(file="l1.stan", 
 	    data=list(N = n, N_delta = N_delta, P=p, ind_sorted_T=ind_sorted_T,
 		      ind_failure_T = ind_failure_T, ind = N_delta:1, X=X, lambda=0.1), 
 	    cores=3, iter=1e4, warmup=1e3)
@@ -34,4 +35,14 @@ stan_diag(m.1)
 stan_dens(m.1)
 get_posterior_mean(m.1)
 get_elapsed_time(m.1)
+
+# horseshoe prior
+m.2 <- stan(file="horseshoe.stan", control = list(adapt_delta = 0.99, max_treedepth=12),
+	    data=list(N = n, N_delta = N_delta, P=p, ind_sorted_T=ind_sorted_T,
+		      ind_failure_T = ind_failure_T, ind = N_delta:1, X=X, lambda=0.1), 
+	    cores=3, iter=2e4, warmup=5e3, chains=2)
+
+traceplot(m.2)
+summary(m.2)
+
 
